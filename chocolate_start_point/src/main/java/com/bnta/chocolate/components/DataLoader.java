04a95dff91fc27403dcdf3de3a902e8f7b368597
Nadia <nadia_dj@hotmail.co.uk>
2022-12-07 16:41:19 +0000

    created data loader and added 3 estates and 3 chocolate objects

---
+++ chocolate_start_point/src/main/java/com/bnta/chocolate/components/DataLoader.java
@@ -0,0 +1,41 @@
+package com.bnta.chocolate.components;
+
+import com.bnta.chocolate.ChocolateApplication;
+import com.bnta.chocolate.models.Chocolate;
+import com.bnta.chocolate.models.Estate;
+import com.bnta.chocolate.repositories.ChocolateRepository;
+import com.bnta.chocolate.repositories.EstateRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataLoader implements ApplicationRunner {
+
+    @Autowired
+    EstateRepository estateRepository;
+
+    @Autowired
+    ChocolateRepository chocolateRepository;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+
+        Estate estate1 = new Estate("BNTA", "Zimbabwe");
+        Estate estate2 = new Estate("Nadia's Estate", "Algeria");
+        Estate estate3 = new Estate("Fatimah's Estate", "India");
+
+
+        Chocolate bounty = new Chocolate("Bounty", 80, estate1);
+        Chocolate maltesers = new Chocolate("Maltesers", 20, estate2);
+        Chocolate galaxy = new Chocolate("Galaxy", 40, estate3);
+
+
+    }
+
+
+
+
+
+}
